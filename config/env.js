daa6dc8dae485cf2d35ffbfc44193e456004913a
Vitto <admin@cxip.dev>
2022-04-02 07:46:57 +0200

    updates

--- config/env.js
+++ config/env.js
@@ -1,10 +1,21 @@
-const env = require('dotenv');
-const path = require('path');
+'use strict';
+
+const env = require ('dotenv');
+const path = require ('path');
 
 // grab root .env file
-env.config({ path: path.join(__dirname, '../.env') });
-// grab local .env file if it exists
-env.config({});
+env.config ({
+    path: path.join (__dirname, '../.env')
+});
 
-module.exports = { NODE_ENV, PRIVATE_KEY, WALLET, MNEMONIC, NETWORK, GAS } =
-  process.env;
+// grab local .env file if it exists
+env.config ({});
+
+module.exports = {
+    NODE_ENV,
+    PRIVATE_KEY,
+    WALLET,
+    MNEMONIC,
+    NETWORK,
+    GAS
+} = process.env;
