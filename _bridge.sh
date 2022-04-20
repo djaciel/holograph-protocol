6d86a2500f5fdf9a9bf6662d8d074d981762b3a9
Vitto <admin@vitto.io>
2022-04-20 18:32:41 +0200

    latest

--- _bridge.sh
+++ _bridge.sh
@@ -2,6 +2,7 @@
 
 node deploy/bridge_1_deploy_sample_erc721.js &&
 node deploy/bridge_2_mint_multichain_nfts.js &&
+node deploy/bridge_3_transfer_multichain_nfts.js &&
 # node deploy/.js &&
 # node deploy/.js &&
 
