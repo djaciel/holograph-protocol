af14132effb25f5c3cb658c395a4c8260423868a
Alexander <alexanderattar@gmail.com>
2023-02-08 15:47:07 -0500

    Update nftCreator to dropCreator

--- src/drops/HolographDropCreatorProxy.sol
+++ src/drops/HolographDropCreatorProxy.sol
@@ -108,35 +108,37 @@ import "../interface/InitializableInterface.sol";
 
 contract HolographDropCreatorProxy is Admin, Initializable {
   /**
-   * @dev bytes32(uint256(keccak256('eip1967.Holograph.nftCreator')) - 1)
+   * @dev bytes32(uint256(keccak256('eip1967.Holograph.dropCreator')) - 1)
    */
-  bytes32 constant _nftCreatorSlot = 0x5f13aaf6ce5a520291ef46f38bbb414a0010a855eabbf6ee02dc27af115fbb5f;
+  bytes32 constant _dropCreatorSlot = 0x8ff08cc6e8c4c508c563852adb1853b353af6edb502f5298dd4937029dcaf6a8;
 
   constructor() {}
 
   function init(bytes memory data) external override returns (bytes4) {
     require(!_isInitialized(), "HOLOGRAPH: already initialized");
-    (address nftCreator, bytes memory initCode) = abi.decode(data, (address, bytes));
+    (address dropCreator, bytes memory initCode) = abi.decode(data, (address, bytes));
     assembly {
       sstore(_adminSlot, origin())
-      sstore(_nftCreatorSlot, nftCreator)
+      sstore(_dropCreatorSlot, dropCreator)
     }
-    (bool success, bytes memory returnData) = nftCreator.delegatecall(abi.encodeWithSignature("init(bytes)", initCode));
+    (bool success, bytes memory returnData) = dropCreator.delegatecall(
+      abi.encodeWithSignature("init(bytes)", initCode)
+    );
     bytes4 selector = abi.decode(returnData, (bytes4));
     require(success && selector == InitializableInterface.init.selector, "initialization failed");
     _setInitialized();
     return InitializableInterface.init.selector;
   }
 
-  function getNFTCreator() external view returns (address nftCreator) {
+  function getDropCreator() external view returns (address dropCreator) {
     assembly {
-      nftCreator := sload(_nftCreatorSlot)
+      dropCreator := sload(_dropCreatorSlot)
     }
   }
 
-  function setNFTCreator(address nftCreator) external onlyAdmin {
+  function setNFTCreator(address dropCreator) external onlyAdmin {
     assembly {
-      sstore(_nftCreatorSlot, nftCreator)
+      sstore(_dropCreatorSlot, dropCreator)
     }
   }
 
@@ -144,9 +146,9 @@ contract HolographDropCreatorProxy is Admin, Initializable {
 
   fallback() external payable {
     assembly {
-      let nftCreator := sload(_nftCreatorSlot)
+      let dropCreator := sload(_dropCreatorSlot)
       calldatacopy(0, 0, calldatasize())
-      let result := delegatecall(gas(), nftCreator, 0, calldatasize(), 0, 0)
+      let result := delegatecall(gas(), dropCreator, 0, calldatasize(), 0, 0)
       returndatacopy(0, 0, returndatasize())
       switch result
       case 0 {
