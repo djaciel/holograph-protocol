c94a17707f9c741e89efef2277a924efc37b3eb6
ACC01ADE <admin@acc01ade.com>
2023-02-12 16:12:06 +0100

    flattening HolographERC721Drop contract

--- contracts/drops/abstract/Initializable.sol
+++ contracts/drops/abstract/Initializable.sol
@@ -101,4 +101,45 @@
 
 pragma solidity 0.8.13;
 
-interface HolographTreasuryInterface {}
+import "../../interface/InitializableInterface.sol";
+
+/**
+ * @title Initializable
+ * @author https://github.com/holographxyz
+ * @notice Use init instead of constructor
+ * @dev This allows for use of init function to make one time initializations without the need for a constructor
+ */
+abstract contract Initializable is InitializableInterface {
+  /**
+   * @dev bytes32(uint256(keccak256('eip1967.Holograph.initialized')) - 1)
+   */
+  bytes32 constant _initializedSlot = 0x4e5f991bca30eca2d4643aaefa807e88f96a4a97398933d572a3c0d973004a01;
+
+  /**
+   * @dev Constructor is left empty and init is used instead
+   */
+  constructor() {}
+
+  /**
+   * @notice Used internally to initialize the contract instead of through a constructor
+   * @dev This function is called by the deployer/factory when creating a contract
+   * @param initPayload abi encoded payload to use for contract initilaization
+   */
+  function init(bytes memory initPayload) external virtual returns (bytes4);
+
+  function _isInitialized() internal view returns (bool initialized) {
+    assembly {
+      initialized := sload(_initializedSlot)
+    }
+  }
+
+  function _setInitialized() internal {
+    assembly {
+      sstore(_initializedSlot, 0x0000000000000000000000000000000000000000000000000000000000000001)
+    }
+  }
+
+  modifier onlyInitializing() {
+    _;
+  }
+}
