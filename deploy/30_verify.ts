d316167143751e83d43d093869868a0a6554a80a
ACC01ADE <admin@acc01ade.com>
2023-05-09 20:42:42 +0200

    verify cleanup

--- deploy/30_verify.ts
+++ deploy/30_verify.ts
@@ -23,7 +23,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const currentNetworkType: NetworkType = networks[hre.network.name].type;
 
   if (currentNetworkType != NetworkType.local) {
-    let contracts: string[] = ['DropsPriceOracleAvalanche'];
+    let contracts: string[] = [
+      'HolographUtilityToken',
+      'hToken',
+      'Holograph',
+      'HolographBridge',
+      'HolographBridgeProxy',
+      'Holographer',
+      'HolographERC20',
+      'HolographERC721',
+      'HolographDropERC721',
+      'HolographDropERC721Proxy',
+      'HolographFactory',
+      'HolographFactoryProxy',
+      'HolographGeneric',
+      'HolographGenesis',
+      'HolographOperator',
+      'HolographOperatorProxy',
+      'HolographRegistry',
+      'HolographRegistryProxy',
+      'HolographTreasury',
+      'HolographTreasuryProxy',
+      'HolographInterfaces',
+      'HolographRoyalties',
+      'CxipERC721',
+      'CxipERC721Proxy',
+      'Faucet',
+      'LayerZeroModule',
+      'EditionsMetadataRenderer',
+      'EditionsMetadataRendererProxy',
+    ];
     for (let i: number = 0, l: number = contracts.length; i < l; i++) {
       let contract: string = contracts[i];
       try {
