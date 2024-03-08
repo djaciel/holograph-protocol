cd39837ccc3a7f00e89c02353c67234dfece888e
Alexander <alexanderattar@gmail.com>
2024-03-08 17:50:22 -0500

    Refactor to make a little more modular

--- scripts/bridge-htokens.ts
+++ scripts/bridge-htokens.ts
@@ -5,8 +5,10 @@ import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import Web3 from 'web3';
 import { GasPricing, initializeGasPricing } from './utils/gas';
+import { string } from 'yargs';
 const web3 = new Web3();
 
+const TEST_GAS_LIMIT: BigNumber = ethers.BigNumber.from('10000000');
 const IS_GAS_ESTIMATION_LOGS_ENABLED = true;
 const IS_OVERRIDE_MOE_GAS_ENABLED = false;
 export const INSUFFICIENT_BALANCE_ERROR = 'Insufficient balance';
@@ -17,15 +19,16 @@ task('bridge-htokens', 'Bridge hToken from one network to another')
   .addParam('token', 'The hToken to bridge')
   .addParam('amount', 'The amount of hToken to bridge')
   .addParam('destination', 'The network to bridge to')
-  // .addParam('from', 'The network to bridge from')
-  // .addParam('to', 'The network to bridge to')
+  .addParam('to', 'The network to bridge to', '')
   .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
-    // const { to, from, amount } = taskArgs; // TODO: allow for from and to to be specified
-    const { destination, amount } = taskArgs;
+    let { destination, amount, to } = taskArgs;
 
     const signer = (await hre.ethers.getSigners())[0]; // Get the first signer
     const from = await signer.getAddress();
-    const to = from; // TODO: Allow for the destination address to be specified
+
+    if (!to || to === '') {
+      to = from;
+    }
 
     // Get the address of the token to bridge
     const network = networks[hre.network.name];
@@ -60,134 +63,50 @@ task('bridge-htokens', 'Bridge hToken from one network to another')
 
     // Estimate the bridging fee
     const result = await bridgeOut(hre, sourceChain, destinationChain, hTokenAddress!, data);
+    console.log(
+      `Finished bridging ${amount} ${taskArgs.token} from ${network.shortKey} to ${destinationChain.shortKey}`
+    );
   });
-
-function getHTokenAddress(network: NetworkType, token: TokenSymbol): string | undefined {
-  return tokenAddresses[network][token];
-}
-
-export const getRpcProvider = (network: Network) => {
-  return new JsonRpcProvider({
-    url: network.rpc,
-  });
-};
-
-export const checkBalanceBeforeTX = (balance: BigNumberish, gas: BigNumberish): string => {
-  if (BigNumber.from(gas).gt(BigNumber.from(balance))) return INSUFFICIENT_BALANCE_ERROR;
-  return '';
-};
 
 async function bridgeOut(
   hre: any,
-  sourceChain: Network,
-  destinationChain: Network,
+  sourceChain: any,
+  destinationChain: any,
   htokenAddress: string,
-  data: BytesLike
+  data: ethers.utils.BytesLike
 ): Promise<any> {
-  const signer = (await hre.ethers.getSigners())[0]; // Get the first signer
-  const holograph = await hre.ethers.getContract('Holograph', signer);
-  const bridgeContract: Contract = await hre.ethers.getContractAt(
-    'HolographBridge',
-    await holograph.getBridge(),
-    signer
-  );
-  const operatorContract: Contract = await hre.ethers.getContractAt('HolographOperator', await holograph.getOperator());
+  const signer = (await hre.ethers.getSigners())[0];
+  const { holograph, bridgeContract, operatorContract } = await initializeContracts(hre, signer);
 
   console.log(`Holgraph contract: ${holograph.address}`);
   console.log(`Bridge contract: ${bridgeContract.address}`);
   console.log(`Operator contract: ${operatorContract.address}`);
   console.log(`H Token address: ${htokenAddress}`);
 
-  // Get the initial payload with max gas limit and gas price to simulate the transaction on destination chain
-  let payload: BytesLike = await bridgeContract.callStatic.getBridgeOutRequestPayload(
+  let payload = await bridgeContract.callStatic.getBridgeOutRequestPayload(
     destinationChain.holographId,
     htokenAddress,
-    '0x' + 'ff'.repeat(32), // Max gas limit
-    '0x' + 'ff'.repeat(32), // Max gas price
-    data as string
+    '0x' + 'ff'.repeat(32),
+    '0x' + 'ff'.repeat(32),
+    data
   );
 
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 1: ', {
-      operatorAddress: operatorContract.address,
-      bridgeAddress: bridgeContract.address,
-      holographToChainId: destinationChain.holographId,
-      address: htokenAddress,
-      initialPayload: payload,
-    });
-  }
+  logEstimation(IS_GAS_ESTIMATION_LOGS_ENABLED, 1, {
+    operatorAddress: operatorContract.address,
+    bridgeAddress: bridgeContract.address,
+    hTokenAddress: htokenAddress,
+    initialPayload: payload,
+  });
 
-  // For proper gas limit estimation, we take the payload and run a simulation on destination chain
-  // load destination chain service provider
   const destinationProvider = getRpcProvider(destinationChain);
-  console.log(destinationProvider);
+  const destinationFrom = getSpecialAddressForChain(destinationChain.chain);
 
-  let destinationFrom = '0x0000000000000000000000000000000000000000';
+  let estimatedGas = await estimateGas(operatorContract, destinationProvider, payload, destinationFrom);
 
-  // Optimism and zora uses a different address for the zero address so we use that instead to simulate the tx while estimating gas
-  // NOTE: The different address is the address of the Wrapped ETH contract
-  if (
-    destinationChain.chain == 10 ||
-    destinationChain.chain == 420 ||
-    destinationChain.chain == 7777777 ||
-    destinationChain.chain == 999999999
-  ) {
-    destinationFrom = '0x4200000000000000000000000000000000000006';
-  }
-
-  // Optimism and zora sepolia uses a different address for the zero address so we use that instead to simulate the tx while estimating gas
-  let sourceFrom = '0x0000000000000000000000000000000000000000';
-  if (
-    sourceChain.chain == 10 ||
-    sourceChain.chain == 420 ||
-    sourceChain.chain == 7777777 ||
-    sourceChain.chain == 999999999
-  ) {
-    sourceFrom = '0x4200000000000000000000000000000000000006';
-  }
-
-  // Arbitrum One uses a different address
-  if (destinationChain.chain == 42161) {
-    destinationFrom = '0xEe01c0CD76354C383B8c7B4e65EA88D00B06f36f';
-  }
-
-  if (sourceChain.chain == 42161) {
-    sourceFrom = '0xEe01c0CD76354C383B8c7B4e65EA88D00B06f36f';
-  }
-
-  // Arbitrum Sepolis uses a different address
-  if (destinationChain.chain == 421614) {
-    destinationFrom = '0x82af49447d8a07e3bd95bd0d56f35241523fbab1';
-  }
-
-  if (sourceChain.chain == 421614) {
-    sourceFrom = '0x82af49447d8a07e3bd95bd0d56f35241523fbab1';
-  }
-
-  // We call jobEstimator on destination chain
-  // by supplying 10 million gas, we get back result of how much gas is left from simulation
-  // subtract leftover gas from 10 million to know exactly how much gas is used for tx
-  // use 10 million gas as base gas to estimate with
-  const testGasLimit: BigNumberish = BigNumber.from('10000000');
-  let estimatedGas: BigNumber = testGasLimit.sub(
-    await operatorContract
-      .connect(destinationProvider)
-      .connect(destinationFrom)
-      .callStatic.jobEstimator(payload as string, {
-        from: destinationFrom,
-        gasLimit: testGasLimit,
-        gasPrice: BigNumber.from(100000000000), // Max gas price
-      })
-  );
-
-  // We extract the most recent and most accurate gas prices for destination network
   const gasPricing: GasPricing = await initializeGasPricing(destinationChain.key, destinationProvider);
+  let destinationGasPrice = gasPricing.isEip1559 ? gasPricing.maxFeePerGas : gasPricing.gasPrice;
 
-  // Get gas price based on eip-1559 support
-  let destinationGasPrice: BigNumber = gasPricing.isEip1559 ? gasPricing.maxFeePerGas! : gasPricing.gasPrice!;
-
-  // Add 50% overhead to accommodate gas spikes and testing
-  destinationGasPrice = destinationGasPrice.add(destinationGasPrice.div(BigNumber.from('2')));
+  destinationGasPrice = destinationGasPrice!.add(destinationGasPrice!.div(2)); // Add 50% overhead
 
   if (
     (destinationChain.chain === networks.polygonTestnet.chain || destinationChain.chain === networks.polygon.chain) &&
@@ -196,72 +115,38 @@ async function bridgeOut(
     destinationGasPrice = MIN_POLYGON_GAS_PRICE;
   }
 
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 2: ', {
-      destinationChainId: destinationChain.chain,
-      destinationFrom: destinationFrom,
-      estimatedGas: estimatedGas,
-      gasPricing: gasPricing,
-      destinationGasPriceBeforeMintGasOverride: BigNumber.from(destinationGasPrice).toString(),
-    });
-  }
+  logEstimation(IS_GAS_ESTIMATION_LOGS_ENABLED, 2, {
+    destinationChainId: destinationChain.chain,
+    estimatedGas: estimatedGas.toString(),
+    gasPricing,
+    destinationGasPrice: destinationGasPrice.toString(),
+  });
 
   // Add 25% to the estimated gas limit for the destination network
-  estimatedGas = estimatedGas.add(estimatedGas.div(BigNumber.from('4')));
+  estimatedGas = estimatedGas.add(estimatedGas.div(4));
 
   if (IS_OVERRIDE_MOE_GAS_ENABLED) {
-    console.log(`Going to set manual values`);
-    estimatedGas = BigNumber.from('1000000'); // set gas limit to 100k wei
-    destinationGasPrice = BigNumber.from('1000000000'); // set gas price to 1 Gwei
+    estimatedGas = ethers.BigNumber.from('1000000'); // set gas limit to 1M wei for override
+    destinationGasPrice = ethers.BigNumber.from('1000000000'); // set gas price to 1 Gwei for override
   }
 
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 3: ', {
-      holographToChainId: destinationChain.holographId,
-      holographToChainIdAsNumber: destinationChain.chain,
-      htokenAddress: htokenAddress,
-      estimatedGas: BigNumber.from(estimatedGas).toString(),
-      destinationGasPrice: BigNumber.from(destinationGasPrice).toString(),
-      estimatedGasHex: BigNumber.from(estimatedGas).toHexString(),
-      destinationGasPriceHex: BigNumber.from(destinationGasPrice).toHexString(),
-      data: data,
-    });
-  }
-
-  // Now that we have gasLimit and gasPrice, we update the payload to include the proper data needed for estimating fees
   payload = await bridgeContract.callStatic.getBridgeOutRequestPayload(
     destinationChain.holographId,
     htokenAddress,
     estimatedGas,
     destinationGasPrice,
-    data as string,
-    {
-      gasLimit: estimatedGas,
-      gasPrice: destinationGasPrice,
-    }
+    data
   );
 
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 4: ', {
-      payload: payload,
-    });
-  }
+  logEstimation(IS_GAS_ESTIMATION_LOGS_ENABLED, 3, { payload });
 
-  // We are now ready to get fees for transaction
-  const fees: BigNumber[] = await bridgeContract.callStatic.getMessageFee(
+  // Prepare for the transaction
+  const fees = await bridgeContract.callStatic.getMessageFee(
     destinationChain.holographId,
     estimatedGas,
     destinationGasPrice,
     payload
   );
-
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 5: ', {
-      hlgFee: BigNumber.from(fees[0]).toString(),
-      msgFee: BigNumber.from(fees[1]).toString(),
-      dstGasPrice: BigNumber.from(fees[2]).toString(),
-    });
-  }
 
   // fees consist of two parts: hlg fee and lz fee
   // fees[0] = hlg fee is the amount that we charge user for making sure operators can get the job done
@@ -271,76 +156,28 @@ async function bridgeOut(
   // for now, to accommodate us time to properly estimate and calculate fees, we add 25% to give us margin for error
   total = total.add(total.div(BigNumber.from('4')));
 
-  // time to run another gas estimate to make sure we accommodate the fact that value is being passed and might affect gas pricing
-  estimatedGas = testGasLimit.sub(
-    await operatorContract
-      .connect(destinationProvider)
-      .connect(destinationFrom)
-      .callStatic.jobEstimator(payload as string, {
-        from: destinationFrom,
-        value: total,
-        gasLimit: testGasLimit,
-      })
-  );
-
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 6: ', {
-      estimatedGas: BigNumber.from(estimatedGas).toString(),
-    });
-  }
-
-  if (IS_OVERRIDE_MOE_GAS_ENABLED) {
-    estimatedGas = BigNumber.from('1000000'); // set gas limit to 1 million wei
-    destinationGasPrice = BigNumber.from('10000000000'); // set gas price to 10 Gwei
-  }
-
-  const unsignedTx: UnsignedTransaction = await bridgeContract.populateTransaction.bridgeOutRequest(
+  const unsignedTx = await bridgeContract.populateTransaction.bridgeOutRequest(
     destinationChain.holographId,
     htokenAddress,
-    estimatedGas, // is gas Limit
+    estimatedGas,
     destinationGasPrice,
-    data as string,
+    data,
     { value: total }
   );
 
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 7: ', {
-      unsignedTx: unsignedTx,
-    });
-  }
+  logEstimation(IS_GAS_ESTIMATION_LOGS_ENABLED, 4, { unsignedTx });
 
-  const balance = await signer.getBalance();
-  const value = total;
-  const gasPrice = await hre.ethers.provider.getGasPrice();
-  const gasLimit = estimatedGas;
-  const gas = gasPrice.mul(gasLimit);
-
-  if (IS_GAS_ESTIMATION_LOGS_ENABLED) {
-    console.log('Bridge gas estimation log 8: ', {
-      balance: BigNumber.from(balance).toString(),
-      value: BigNumber.from(value).toString(),
-      gasPrice: BigNumber.from(gasPrice).toString(),
-      gasLimit: BigNumber.from(gasLimit).toString(),
-      gas: BigNumber.from(gas).toString(),
-    });
-  }
-
-  const error = checkBalanceBeforeTX(balance, gas.add(value));
-
-  // TODO: Move this to it's own function
-  const tx = await signer.sendTransaction(unsignedTx as Object);
+  // Send the transaction
+  const tx = await signer.sendTransaction(unsignedTx);
   await tx.wait();
 
   console.log(`Transaction hash: ${tx.hash}`);
 
   return {
-    gasPrice,
-    gasLimit,
-    gas,
-    value,
-    total: gas.add(value),
-    unsignedTx,
-    error,
+    txHash: tx.hash,
+    estimatedGas,
+    destinationGasPrice,
+    total,
   };
 }
 
@@ -372,5 +209,60 @@ export const tokenAddresses: HTokenAddresses = {
 const generateInitCode = function (vars: string[], vals: any[]): string {
   return web3.eth.abi.encodeParameters(vars, vals);
 };
+
+function getHTokenAddress(network: NetworkType, token: TokenSymbol): string | undefined {
+  return tokenAddresses[network][token];
+}
+
+export const getRpcProvider = (network: Network) => {
+  return new JsonRpcProvider({
+    url: network.rpc,
+  });
+};
+
+export const checkBalanceBeforeTX = (balance: BigNumberish, gas: BigNumberish): string => {
+  if (BigNumber.from(gas).gt(BigNumber.from(balance))) return INSUFFICIENT_BALANCE_ERROR;
+  return '';
+};
+
+// Helper to initialize necessary contracts
+async function initializeContracts(hre: any, signer: ethers.Signer) {
+  const holograph = await hre.ethers.getContract('Holograph', signer);
+  const bridgeContract = await hre.ethers.getContractAt('HolographBridge', await holograph.getBridge(), signer);
+  const operatorContract = await hre.ethers.getContractAt('HolographOperator', await holograph.getOperator());
+  return { holograph, bridgeContract, operatorContract };
+}
+
+// Helper for logging
+function logEstimation(logEnabled: boolean, step: number, data: object) {
+  if (logEnabled) {
+    console.log(`Bridge gas estimation log ${step}: `, data);
+  }
+}
+
+// Helper to determine special addresses for specific chains
+function getSpecialAddressForChain(chainId: number): string {
+  const specialAddresses: { [key: number]: string } = {
+    10: '0x4200000000000000000000000000000000000006',
+    420: '0x4200000000000000000000000000000000000006',
+    42161: '0xEe01c0CD76354C383B8c7B4e65EA88D00B06f36f',
+    421614: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+  };
+  return specialAddresses[chainId] || '0x0000000000000000000000000000000000000000';
+}
+
+async function estimateGas(
+  operatorContract: Contract,
+  destinationProvider: any,
+  payload: BytesLike,
+  destinationFrom: string
+): Promise<BigNumber> {
+  return TEST_GAS_LIMIT.sub(
+    await operatorContract.connect(destinationProvider).connect(destinationFrom).callStatic.jobEstimator(payload, {
+      from: destinationFrom,
+      gasLimit: TEST_GAS_LIMIT,
+    })
+  );
+}
 
 export default {};
